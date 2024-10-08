3cb1e06d62e35776cdc113e9783e46042c70a16b
ev-eve <em16me@gmail.com>
2024-10-08 12:11:16 +0300

    Сравнение с ботом 2.0

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         Bot
+// @name         BotBing
 // @namespace    http://tampermonkey.net/
-// @version      2.0
-// @description  Bot for google
-// @author       Sergey Chizhikov
-// @match        https://www.google.com/*
+// @version      1.0
+// @description  Bot for bing
+// @author       Evgeniy Titov
+// @match        https://www.bing.com/*
 // @match        https://napli.ru/*
 // @match        https://kiteuniverse.ru/*
 // @match        https://motoreforma.com/*
@@ -14,9 +14,9 @@
 
 let input = document.getElementsByName("q")[0];
 let links = document.links;
-let searchBtn = document.getElementsByName("btnK")[0];
+let searchBtn = document.getElementsByName("search")[0];
 let sites = {
-  "napli.ru": ["вывод произвольных полей wordpress", "10 самых популярных шрифтов от Google", 
+  "napli.ru": ["вывод произвольных полей wordpress", "10 самых популярных шрифтов от Google",
                "Отключение редакций и ревизий в WordPress", "Базовые вещи про GIT"],
   "kiteuniverse.ru": ["Шоу воздушных змеев", "Kite Universe Россия", "Воздушные змеи – наша специализация"],
   "motoreforma.com": ["тюнинг Maverick", "прошивки CAN-AM", "запчасти для квадроциклов"],
@@ -29,13 +29,16 @@ let keyword = keywords[getRandom(0, keywords.length)];
 
 if (searchBtn !== undefined) {
   document.cookie = `site=${site}`;
-} else if (location.hostname == "www.google.com") {
-  site = getCookie("site")
+} else if (location.hostname == "www.bing.com") {
+  site = getCookie("site");
 } else {
   site = location.hostname;
 }
 
-//Работаем на главной странице поисковика
+// Работаем на главной странице поисковика, отключение автозаполнения input Bing:
+// https://answers.microsoft.com/en-us/bing/forum/all/how-to-disable-bing-suggestions-when-i-search/aebd0069-f1cb-4a69-a3b2-17f785ad3e76
+
+
 if (searchBtn !== undefined) {
   let i = 0;
   let timerId = setInterval(function() {
@@ -48,23 +51,21 @@ if (searchBtn !== undefined) {
   }, 300)
 
   } else if (location.hostname == site) {
-    setInterval(() => {
+    setInterval (() => {
       let index = getRandom(0, links.length);
       let localLink = links[index];
 
       if (getRandom(0, 101) >= 70) {
-        location.href = "https://www.google.com/";
+        location.href = "https://www.bing.com/";
       }
 
-      if(localLink.href.includes(site)) {
-        localLink.click();
+      if (localLink.href.includes(site)) {
+        localLink.click()
       }
-
-    }, getRandom(3000, 5000))
-
+    }, getRandom(3000, 5000)) //3-5 seconds
     console.log("Мы на целевом сайте");
   }
-//Работаем на странице поисковой выдачи
+//Работаем на странице поисковой выдачи  
 else {
   let nextPage = true;
   for (let i = 0; i < links.length; i++) {
@@ -78,13 +79,13 @@ else {
       break;
     }
   }
-  if (document.querySelector(".YyVfkd").innerText == "5") {
+  if (document.querySelector(".sb_pagS").innerText == "5") {
     nextPage = false;
-    location.href = "https://www.google.com/";
+    location.href = "https://www.bing.com/";
   }
   if (nextPage) {
     setTimeout(()=>{
-      pnnext.click();
+      document.querySelector(".sb_pagN").click();
     }, getRandom(2500, 4000))
 
   }
@@ -93,6 +94,7 @@ else {
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
+
 function getCookie(name) {
   let matches = document.cookie.match(new RegExp(
     "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
